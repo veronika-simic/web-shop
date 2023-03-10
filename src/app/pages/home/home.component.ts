d6fa3242cc36e8cf83c9bab1cb08dd8ad3e3e844
veronika-simic <veronikasimic21@gmail.com>
2023-03-10 08:36:31 +0100

    basic layout for home page

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,11 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
-  template: `
-    <p>
-      home works!
-    </p>
-  `,
+  templateUrl: './home.component.html',
   styles: [
   ]
 })
