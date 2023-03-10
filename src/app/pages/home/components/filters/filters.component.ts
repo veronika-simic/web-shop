45e3b2f248b4389267055794326020c49c8e2e9d
veronika-simic <veronikasimic21@gmail.com>
2023-03-10 10:21:45 +0100

    add filters component

---
+++ src/app/pages/home/components/filters/filters.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-filters',
+  templateUrl: './filters.component.html' 
+})
+export class FiltersComponent {
+  categories = ['ranchera', 'cumbia', 'pop']
+}
