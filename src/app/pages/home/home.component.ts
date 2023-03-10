3317076ede2cf71d108b3fcec788552249341cf4
veronika-simic <veronikasimic21@gmail.com>
2023-03-10 10:51:31 +0100

    dynamically change row height

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,16 +1,18 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
+const ROWS_HEIGHTS: { [id: number]: number } = { 1: 400, 3: 335, 4: 350 };
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
+  selector: "app-home",
+  templateUrl: "./home.component.html",
 })
 export class HomeComponent {
- 
   cols = 3;
-  category: string | undefined
+  rowHeight = ROWS_HEIGHTS[this.cols];
+  category: string | undefined;
 
   onColumnsCountChange(colsNum: number): void {
-    this.cols = colsNum
+    this.cols = colsNum;
+    this.rowHeight = ROWS_HEIGHTS[this.cols];
   }
 
   onCategoryChange(category: string): void {
