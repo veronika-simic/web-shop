8b1a0f17993a4d0211179f0aa96155503751b78f
veronika-simic <veronikasimic21@gmail.com>
2023-03-11 06:52:37 +0100

    add routing for cart page

--- src/app/pages/cart/cart.component.ts
+++ src/app/pages/cart/cart.component.ts
@@ -2,13 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-cart',
-  template: `
-    <p>
-      cart works!
-    </p>
-  `,
-  styles: [
-  ]
+  templateUrl: './cart.component.html',
 })
 export class CartComponent {
 
