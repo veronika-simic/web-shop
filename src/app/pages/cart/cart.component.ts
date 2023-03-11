6081d256c074aefd13b173494009520342c97b97
veronika-simic <veronikasimic21@gmail.com>
2023-03-11 06:58:45 +0100

    add dumy products into cart

--- src/app/pages/cart/cart.component.ts
+++ src/app/pages/cart/cart.component.ts
@@ -1,9 +1,27 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { Cart } from "app/models/cart.model";
 
 @Component({
-  selector: 'app-cart',
-  templateUrl: './cart.component.html',
+  selector: "app-cart",
+  templateUrl: "./cart.component.html",
 })
 export class CartComponent {
-
+  cart: Cart = {
+    items: [
+      {
+        product: "https://via.placeholder.com/150",
+        name: "Te regalo la lluvia",
+        price: 20,
+        quantity: 1,
+        id: 2,
+      },
+      {
+        product: "https://via.placeholder.com/150",
+        name: "Arrasando",
+        price: 22,
+        quantity: 3,
+        id: 1,
+      },
+    ],
+  };
 }
